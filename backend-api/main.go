141952634e10a68d6beed2f9a9b35054ba6cc6fd
firdhausranggaa <ranzaqfirdhaus@gmail.com>
2026-09-09 23:12:04 +0700

    style: menyesuaikan format import agar konsisten di main.go

--- backend-api/main.go
+++ backend-api/main.go
@@ -1,15 +1,15 @@
 package main
 
 import (
-	"gocommerce/configs"
-	"gocommerce/handlers"
-	"gocommerce/middlewares"
-	"gocommerce/migrations"
-	_ "net/http/pprof"
+    "gocommerce/configs"
+    "gocommerce/handlers"
+    "gocommerce/middlewares"
+    "gocommerce/migrations"
+    "gocommerce/models"
+    _ "net/http/pprof"
 
-	"github.com/gin-contrib/cors"
-
-	"github.com/gin-gonic/gin"
+    "github.com/gin-contrib/cors"
+    "github.com/gin-gonic/gin"
 )
 
 func main() {
